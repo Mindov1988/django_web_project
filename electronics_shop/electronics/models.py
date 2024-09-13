3919bc3ba464ea6479039544d96867f327e9b1fa
Georgi Mindov <georgi.mindoff@gmail.com>
2024-09-13 14:31:30 +0300

    change hardcoded values from models

--- electronics_shop/electronics/models.py
+++ electronics_shop/electronics/models.py
@@ -4,16 +4,21 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    NAME_MAX_LENGTH = 255
+    name = models.CharField(max_length=NAME_MAX_LENGTH)
     description = models.TextField()
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
+    NAME_MAX_LENGTH = 255
+    MAX_PRICE_DIGITS = 10
+    MAX_DECIMAL_PLACES = 2
+
+    name = models.CharField(max_length=NAME_MAX_LENGTH)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=MAX_PRICE_DIGITS, decimal_places=MAX_DECIMAL_PLACES)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
 
@@ -21,9 +26,12 @@ class Product(models.Model):
         return self.name
 
 class Order(models.Model):
+    MAX_TOTAL_PRICE_DIGITS = 10
+    MAX_DECIMAL_PLACES = 2
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=MAX_TOTAL_PRICE_DIGITS, decimal_places=MAX_DECIMAL_PLACES)
     cart_data = models.JSONField(default=dict)  # To store cart items
 
     def __str__(self):
@@ -40,6 +48,9 @@ class Review(models.Model):
         return f'Review of {self.product.name} by {self.user.username}'
 
 class Customer(models.Model):
+    MAX_ADDRESS_LENGTH = 255
+    MAX_PHONE_LENGTH = 15
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=255)
-    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=MAX_ADDRESS_LENGTH)
+    phone = models.CharField(max_length=MAX_PHONE_LENGTH)
